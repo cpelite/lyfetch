ed9b1e6d8a8395a499f707924cd5eb9e955a219b
CPElite <cpelite@zlatina.tech>
2023-11-29 10:54:32 +0100

    almost finished

--- lyfetch.sh
+++ lyfetch.sh
@@ -15,3 +15,9 @@ available_memory="$(free --giga -h | awk 'FNR == 2 {print $4}')"
 echo "| Memory: $total_memory"
 echo "| Used memory: $used_memory"
 echo "| Available memory: $available_memory"
+
+processor_name="$(grep 'model name' /proc/cpuinfo | head -n 1 | cut -d ':' -f 2 | tr -s ' ')"
+processor_cores="$(nproc --all)"
+
+echo "| Processor name: $processor_name"
+echo "| Available cores: $processor_cores"
